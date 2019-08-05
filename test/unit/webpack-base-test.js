a6f6e209d7720380f6323a66707ba4398ff565a6
haoqi <290058005@qq.com>
2019-08-05 22:29:21 +0800

    x

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,8 +4,8 @@ const assert = require('assert')
 
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
-
+    console.log(baseConfig.entry.index)
     it('entry', () => {
-        assert.equal(baseConfig.entry.index.endsWith('uilder-webpack/test/smoke/template/src/index/index.js'), true);
+        assert.equal(baseConfig.entry.index.endsWith('/smoke/template/src/index/index.js'), true);
     });
 });
